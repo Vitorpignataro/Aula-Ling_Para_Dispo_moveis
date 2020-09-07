726836a4c4495b5f448bbf8e1ec8543756c44cf1
José Vitor <josevitorpignataro@gmail.com>
2020-09-07 16:44:06 -0300

    Edição de alguns conteúdos

--- Pinh4taPizzaria/app/src/main/java/com/example/pinh4tapizzaria/Pinh4ta_pizzaria.java
+++ Pinh4taPizzaria/app/src/main/java/com/example/pinh4tapizzaria/Pinh4ta_pizzaria.java
@@ -1,3 +1,7 @@
+//José vitor alves Pignataro
+//96150
+//Sexto Semestre
+
 package com.example.pinh4tapizzaria;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,22 +19,24 @@ public class Pinh4ta_pizzaria extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pinh4ta_pizzaria);
-
+        //declaração do botão registrar e o texto vazio que reage com o click dos botões
         Button buttonRegis = (Button) findViewById(R.id.buttonRegistrar);
         final TextView notifica = (TextView) findViewById(R.id.notificaoRegistropedido);
 
         buttonRegis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //ao clicar no buttonRegis ele chama o textview abaixo
                 notifica.setText("Registando Pedido");
             }
         });
-
+        //declaração do botão responsável pelas horas
         Button burronTlhoras = (Button) findViewById(R.id.buttonTotalhoras);
 
         burronTlhoras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //ao clicar no burronTlhoras ele chama o textview abaixo
                 notifica.setText("Total De Horas");
             }
         });
